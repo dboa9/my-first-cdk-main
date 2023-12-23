bc171d3abe789222a01203534e691ffbca4d5541
dboa9 <danielalchemy9@gmail.com>
2023-12-23 16:50:05 +0000

    add cicd & test

---
+++ test/my-first-cdk-app.test.ts
@@ -0,0 +1,16 @@
+import { handler } from "../src";
+
+describe('My First CDK App Tests', () => {
+
+    it('returns name and tekst', async () => {
+    const output = await handler({name: 'Faruk'});
+    expect(output).toStrictEqual('Good Job Faruk!')   
+    
+})
+
+it('returns no name and tekst', async () => {
+    const output = await handler({name: ''});
+    expect(output).toStrictEqual('Good job')
+})
+
+});
